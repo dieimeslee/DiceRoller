376a36771e6391266c9c9a62e93756d1d717083d
Dieimes <andrec_s@yahoo.com.br>
2022-07-01 01:19:54 -0300

    primeiro commit

---
+++ dice_roller.py
@@ -0,0 +1,26 @@
+# 6 sided die roll and present a number
+import random
+
+class Funcionamento:
+    def __init__(self):
+        self.valor_minimo = 1
+        self.valor_maximo = 6
+        self.mensagem = 'Rolar o dado novamente?(sim/não): '
+
+    def Iniciar(self):
+        escolha = input(self.mensagem).strip().lower()
+        while escolha != 'não':
+            if escolha == 'sim':
+                self.RolarDado()
+                escolha = input(self.mensagem).strip().lower()
+            else:
+                print('Opção inválida')
+                escolha = input(self.mensagem).strip().lower()
+        print('Ok, obrigado!')
+
+    def RolarDado(self):
+        print(random.randint(self.valor_minimo, self.valor_maximo))
+
+rodar_dados = Funcionamento()
+rodar_dados.Iniciar()
+print('teste')
